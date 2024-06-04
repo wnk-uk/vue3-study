4cb8f4e73e90ecde5ea8d416511124e806c5fa29
wnk-uk <dailycoding0211@gmail.com>
2024-06-04 19:21:06 +0900

    form add

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form action="" @submit.prevent="submitForm">
+      <div>
+          <label for="userId">ID: </label>
+          <input id="userId" type="text" v-model="username">
+      </div>
+      <div>
+          <label for="password">PW: </label>
+          <input id="password" type="text" v-model="password">
+      </div>
+      <button type="submit">로그인</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  //import 라이브러리이름 form '패키지이름'
+  import axios from 'axios';
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    // data() {
+    //   return {
+    //     username : '',
+    //     password: '',
+    //   }
+    // },
+    // methods: {
+    //     submitForm() {
+    //       // event.preventDefault();
+    //       //fake api
+    //       const data = {
+    //         username: this.username,
+    //         password: this.password,
+    //       }
+    //       axios.post('https://jsonplaceholder.typicode.com/users/', data)
+    //       .then(response => {
+    //         console.log(response);
+    //       })
+    //       //console.log('제출됨');
+    //     }
+    //   }
+    //컴포지션 api
+      setup() {
+        // data
+        var username = ref('');
+        var password = ref('');
+
+        var submitForm = () => { 
+          axios.post('https://jsonplaceholder.typicode.com/users/', {
+            username: username.value,
+            password: password.value
+          }).then(response => {
+            console.log(response);
+          })
+        }
+
+        return { username, password, submitForm }
+      },
+      methods: {
+        logText() {
+
+        }
+      }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
